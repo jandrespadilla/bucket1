cf4e46accc3291436df45c380a973278006bb152
Jorge Andres Padilla <jandrespadilla@gmail.com>
2022-05-16 06:37:33 -0300

    usando doker

--- index.php
+++ index.php
@@ -1,46 +1,41 @@
-<?php
-require_once './vendor/autoload.php';
-
-use Aws\S3\S3Client;
-use Awd\Exception\S3Exception;
-
-
-if (isset($_FILE['file'])) {
-    s3_upload_put_object($_FILE['file']);
-}
-
-function s3_upload_put_object($file){
-
-    $option = [
-        'region' => 'us-west-2',
-        'version' => '2006-03-01',
-        'credential' => [
-            'key' => '',
-            'secret' => '',
-        ]
-
-        ];
-
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload S3
-    </title>
+<title>Show databases in MySQL server</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
-    <form action="" method='post' enctype='multipart/form-data' >
-        <label for="file"> Subir archivo</label>
-        <input type="file" name='file' id='file'>
-        <button type='submit'> Subir archivo </button>
-    </form>
+<div class="container">
+<h1>Show databases in MySQL server</h1>
+<?php
+
+getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="localhost";
+getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3306";
+getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
+getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="secret";
+getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="";
+
+if (strlen( $db_name ) === 0)
+  $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass);
+else 
+  $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass, $db_name);
+
+// Check connection
+if ($conn->connect_error) 
+	die("Connection failed: " . $conn->connect_error);
+ 
+if (!($result=mysqli_query($conn,'SHOW DATABASES')))
+    printf("Error: %s\n", mysqli_error($conn));
+
+echo "<h3>Databases</h3>";
+
+while($row = mysqli_fetch_row( $result ))
+    echo $row[0]."<br />";
+
+$result -> free_result();
+$conn->close();
+?>
+</div>
 </body>
 </html>
